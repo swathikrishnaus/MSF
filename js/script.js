c97059d1d581942b8c8de63833a6f7301b68d645
swathikrishnaus <swathikrishnaus2015@gmail.com>
2023-02-23 19:06:46 +0530

    addded

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
+
+$(document).ready(function() {
+	var url = window.location.pathname;
+	var activePage = url.substring(url.lastIndexOf('/')+1);
+	
+	$('.nav-link').each(function() {
+	  var linkPage = this.href.substring(this.href.lastIndexOf('/')+1);
+	  
+	  if (activePage == linkPage) {
+		$(this).addClass('active');
+	  }
+	});
+  });
+
+
+
+
+
 $(document).ready(function(){
 	$('.venobox').venobox();
 	$('.my-video-links').venobox();
 	$('.my-video-gallery').venobox();
 });
+
+
+
+
+
+
+
